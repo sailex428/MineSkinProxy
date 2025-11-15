22da13c09b8869019d0c9d434fc1b0b1a757ae1d
sailex <elias.hahn@mail.de>
2025-11-15 01:51:15 +0100

    build: add deps required by javalin

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
     implementation("org.mineskin:java-client:3.0.6")
     implementation("org.mineskin:java-client-java11:3.0.6")
     implementation("io.javalin:javalin:6.7.0")
+    implementation("org.slf4j:slf4j-simple:2.0.17")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.1")
 }
 
 kotlin {
