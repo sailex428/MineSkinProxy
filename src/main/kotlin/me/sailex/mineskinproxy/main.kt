050a22b3c91a355f20bf2e5b5ceca1403783c21e
sailex <elias.hahn@mail.de>
2025-11-15 01:49:53 +0100

    feat: add main class

--- src/main/kotlin/me/sailex/mineskinproxy/main.kt
+++ src/main/kotlin/me/sailex/mineskinproxy/main.kt
@@ -1,7 +1,12 @@
 package me.sailex.mineskinproxy
 
-
-
 fun main(args : Array<String>) {
-
+    val apiKey = System.getenv("MINE_SKIN_API_KEY")
+    val port = System.getenv("API_PORT")?.toInt() ?: 8080
+    val userAgent = System.getenv("USER_AGENT")?: "MineSkinProxy"
+    if (apiKey != null) {
+        MineSkinAPI(apiKey, userAgent).startListening(port)
+    } else {
+        println("MINE_SKIN_API_KEY not set")
+    }
 }
