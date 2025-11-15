3705a4d2ddc0ed4d278d065744e032e3237c821d
sailex <elias.hahn@mail.de>
2025-11-15 01:49:21 +0100

    feat: add mineSkinApi

---
+++ src/main/kotlin/me/sailex/mineskinproxy/MineSkinAPI.kt
@@ -0,0 +1,77 @@
+package me.sailex.mineskinproxy
+
+import com.fasterxml.jackson.core.JacksonException
+import com.google.gson.Gson
+import io.javalin.Javalin
+import org.mineskin.Java11RequestHandler
+import org.mineskin.MineSkinClient
+import org.mineskin.data.CodeAndMessage
+import org.mineskin.data.JobInfo
+import org.mineskin.data.Variant
+import org.mineskin.data.Visibility
+import org.mineskin.exception.MineSkinRequestException
+import org.mineskin.request.GenerateRequest
+import java.util.Optional
+
+class MineSkinAPI(
+    private val apiKey: String,
+    private val userAgent: String
+) {
+    private val gson = Gson()
+    private val app: Javalin = Javalin.create()
+
+    init {
+        val client: MineSkinClient = MineSkinClient.builder()
+            .apiKey(apiKey)
+            .requestHandler(::Java11RequestHandler)
+            .userAgent(userAgent)
+            .build()
+        defineEndpoint(client)
+    }
+
+    fun startListening(port: Int) {
+        app.start(port)
+    }
+
+    private fun defineEndpoint(client: MineSkinClient) {
+        app.post("/skin") { ctx ->
+            var request: SkinRequest? = null
+            try {
+                request = ctx.bodyAsClass(SkinRequest::class.java)
+            } catch (e: JacksonException) {
+                println(e)
+                ctx.status(400).result("Invalid request body: ${e.message}")
+            }
+
+            val requestBody: GenerateRequest = GenerateRequest.url(request?.url)
+                .visibility(Visibility.UNLISTED)
+
+            val result = client.queue().submit(requestBody).thenCompose { queueResponse ->
+                val job: JobInfo = queueResponse.job
+                job.waitForCompletion(client)
+            }.thenCompose { jobResponse -> jobResponse.getOrLoadSkin(client) }
+            .thenApply { skinInfo ->
+                SkinResult(
+                    skinInfo.texture().data().value(),
+                    skinInfo.texture().data().signature(),
+                    request?.variant ?: Variant.AUTO,
+                )
+            }
+            .exceptionally { throwable ->
+                if (throwable is MineSkinRequestException) {
+                    val detailsOptional: Optional<CodeAndMessage> = throwable.response.errorOrMessage
+                    detailsOptional.ifPresent { details ->
+                        ctx.status(500).result(details.code() + ": " + details.message())
+                    }
+                }
+                null
+            }.join()
+
+            if (result != null) {
+                ctx.status(200).result(gson.toJson(result))
+            } else {
+                ctx.status(400).result("No result")
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/me/sailex/mineskinproxy/SkinResult.kt
@@ -0,0 +1,5 @@
+package me.sailex.mineskinproxy
+
+import org.mineskin.data.Variant
+
+data class SkinResult(val signature: String, val texture: String, val variant: Variant)
